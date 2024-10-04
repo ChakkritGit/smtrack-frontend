62bbc415a855365713d6ee9224f9ae7fa1de3b1b
ChakkritGit <nongtonnee@gmail.com>
2024-10-04 12:40:46 +0700

    update

--- src/pages/policy/policy.tsx
+++ src/pages/policy/policy.tsx
@@ -4,14 +4,11 @@ function Policy() {
   return (
     <PrivacyContainer>
       <HeadTitle>Privacy Policy</HeadTitle>
-      <br />
       <span>This privacy policy applies to the SMTrack+ app (hereby referred to as "Application") for mobile devices that was created by THANES DEVELOPMENT COMPANY LIMITED (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
       </span>
       <BR />
       <HeadTitle>Information Collection and Use</HeadTitle>
-      <br />
       <span>he Application collects information when you download and use it. This information may include information such as</span>
-      <br />
       <ul>
         <li>Your device's Internet Protocol address (e.g. IP address)</li>
         <li>The pages of the Application that you visit, the time and date of your visit, the time spent on those pages</li>
@@ -21,13 +18,10 @@ function Policy() {
       <span>
         The Application does not gather precise information about the location of your mobile device.
       </span>
-      <br />
       <span>The Service Provider may use the information you provided to contact you from time to time to provide you with important information, required notices and marketing promotions.</span>
-      <br />
       <span>For a better experience, while using the Application, the Service Provider may require you to provide us with certain personally identifiable information. The information that the Service Provider request will be retained by them and used as described in this privacy policy.</span>
       <BR />
       <HeadTitle>Third Party Access</HeadTitle>
-      <br />
       <span>Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Service Provider may share your information with third parties in the ways that are described in this privacy statement.
       </span>
       <BR />
@@ -39,35 +33,28 @@ function Policy() {
         <li>with their trusted services providers who work on their behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.</li>
       </ul>
       <HeadTitle>Opt-Out Rights</HeadTitle>
-      <br />
       <span>You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.</span>
       <BR />
       <HeadTitle>Data Retention Policy</HeadTitle>
-      <br />
       <span>The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at siamatic.thanesgroup@gmail.com and they will respond in a reasonable time.</span>
       <BR />
       <HeadTitle>Children</HeadTitle>
-      <br />
       <span>The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13.</span>
       <BR />
       <span>The Application does not address anyone under the age of 13. The Service Provider does not knowingly collect personally identifiable information from children under 13 years of age. In the case the Service Provider discover that a child under 13 has provided personal information, the Service Provider will immediately delete this from their servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact the Service Provider (siamatic.thanesgroup@gmail.com) so that they will be able to take the necessary actions.</span>
       <BR />
       <HeadTitle>Security</HeadTitle>
-      <br />
       <span>The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect information the Service Provider processes and maintains.</span>
       <BR />
       <HeadTitle>Changes</HeadTitle>
-      <br />
       <span>This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to the Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.</span>
       <BR />
       <span>This privacy policy is effective as of 2024-10-04</span>
       <BR />
       <HeadTitle>Your Consent</HeadTitle>
-      <br />
       <span>By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.</span>
       <BR />
       <HeadTitle>Contact Us</HeadTitle>
-      <br />
       <span>If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at siamatic.thanesgroup@gmail.com.</span>
     </PrivacyContainer>
   )
--- src/style/components/policy.ts
+++ src/style/components/policy.ts
@@ -1,14 +1,24 @@
 import styled from "styled-components"
 
 export const PrivacyContainer = styled.div<{$primary?: boolean}>`
+  display: flex;
+  flex-direction: column;
   color: ${props => props.theme.mode === 'dark' ? 'var(--white)' : 'var(--black)'};
   padding: 1rem;
   max-width: 720px;
   margin: 1rem auto 3rem auto;
+
+  &>ul {
+    margin-top: .5rem;
+  }
+
+  &>ul>li {
+    margin-bottom: .5rem;
+  }
 `
 
 export const BR = styled.div `
-margin: 1.7rem 0;
+  margin: 1.125rem 0;
 `
 
 export const HeadTitle = styled.span`
