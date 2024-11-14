c334825736fcb05312fab1f2f314c0c8b7f5ee20
ChakkritGit <nongtonnee@gmail.com>
2024-11-14 12:06:46 +0700

    update

--- src/style/components/home.styled.ts
+++ src/style/components/home.styled.ts
@@ -128,7 +128,7 @@ export const NotiActionFlex = styled.div<{ $primary?: boolean }>`
   padding: 1rem;
     `}
 
-  & > div:nth-child(1) {
+  & > div:nth-child(2) {
     & > button:nth-child(2) {
       background-color: var(--main-color);
       padding: .5rem .7rem;
